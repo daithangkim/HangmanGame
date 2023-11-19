26e4349892b3a76d56136a3b90feaddd93529be0
daithangkim <88834424+daithangkim@users.noreply.github.com>
2023-11-19 12:06:15 +0100

    word gaps, word-list, creating random words and displaying it with a hint

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,31 @@
 const wordDisplay = document.querySelector(".word-display");
 const keyboardDiv = document.querySelector(".keyboard");
 
+let currentWord;
+
 //creating random words and their word display gaps
 const getRandomWord = () => {
     const {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(word, hint);
+    currentWord = word;
+    console.log(word);
     document.querySelector(".Hinweis b").innerText = hint;
     wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
 }
 
-//when clicked...
 const initGame = (button, clickedLetter) => {
-    console.log(button, clickedLetter);
-
+    //check whether letter exists when clicked
+    if (currentWord.includes(clickedLetter)) {
+        console.log(clickedLetter, " exists in the word")
+    } else {
+        console.log(clickedLetter, " exists not in the word")
+    }
 }
 //creating keyboard buttons and adding event listeners
 for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
     keyboardDiv.appendChild(button);
-    button.addEventListener("click", e=> initGame(e.target, String.fromCharCode(i)))
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)))
 }
 
 getRandomWord();
